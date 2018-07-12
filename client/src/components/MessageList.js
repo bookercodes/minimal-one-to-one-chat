54cee4216300b15eb8c7cdf063c259fa3f627ddc
Alex Booker <booker@booker.codes>
2018-07-12 11:15:59 +0100

    Clean up message list

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,18 +1,62 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { css } from 'emotion';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { css } from 'emotion'
+import Avatar from './Avatar'
+
+const Message = ({ message }) => (
+  <li
+    className={css({
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingTop: 9,
+      paddingBottom: 9
+    })}
+  >
+    <Avatar
+      userId={message.sender.id}
+      className={css({
+        width: 32,
+        height: 32,
+        marginRight: 6
+      })}
+    />
+    <div
+      className={css({
+        display: 'flex',
+        flexDirection: 'column'
+      })}
+    >
+      <span
+        className={css({
+          fontSize: '.8em',
+          fontWeight: 'bold',
+          marginBottom: 2
+        })}
+      >
+        {message.sender.id.toLowerCase()}
+      </span>
+      <span className={css({ flex: 1, fontSize: '.8em' })}>
+        {message.text}
+      </span>
+    </div>
+  </li>
+)
+
 class MessageList extends Component {
   componentWillUpdate() {
-    const node = ReactDOM.findDOMNode(this);
+    const list = ReactDOM.findDOMNode(this)
     this.shouldScrollToBottom =
-      node.scrollTop + node.clientHeight + 100 >= node.scrollHeight;
+      list.scrollTop + list.clientHeight + 100 >= list.scrollHeight
   }
+
   componentDidUpdate() {
     if (this.shouldScrollToBottom) {
-      const node = ReactDOM.findDOMNode(this);
-      node.scrollTop = node.scrollHeight;
+      const list = ReactDOM.findDOMNode(this)
+      list.scrollTop = list.scrollHeight
     }
   }
+
   render() {
     return (
       <ul
@@ -23,49 +67,11 @@ class MessageList extends Component {
         })}
       >
         {this.props.messages.map(message => (
-          <li
-            className={css({
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              paddingTop: 9,
-              paddingBottom: 9
-            })}
-          >
-            <img
-              src={`https://ui-avatars.com/api/?name=${
-                message.sender.id
-              }`}
-              className={css({
-                borderRadius: 5,
-                width: 32,
-                height: 32,
-                marginRight: 6
-              })}
-            />
-            <div
-              className={css({
-                display: 'flex',
-                flexDirection: 'column'
-              })}
-            >
-              <span
-                className={css({
-                  fontSize: '.8em',
-                  fontWeight: 'bold',
-                  marginBottom: 2
-                })}
-              >
-                {message.sender.id.toLowerCase()}
-              </span>
-              <span className={css({ flex: 1, fontSize: '.8em' })}>
-                {message.text}
-              </span>
-            </div>
-          </li>
+          <Message message={message} />
         ))}
       </ul>
-    );
+    )
   }
 }
-export default MessageList;
+
+export default MessageList
